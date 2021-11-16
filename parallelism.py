031c5277ccd56e01376f0d798fb658e196b9ee4b
Vignesh_S <snesh93@gmail.com>
2021-11-16 15:57:22 +0100

    Added the parallism platform code

--- parallelism.py
+++ parallelism.py
@@ -3,12 +3,21 @@
 import threading
 import multiprocessing
 import serial
+from sys import platform
 
 
-def portCommunication(portName):
+def portCommunication(port):
     with serial.Serial(portName, 921600, timeout=3) as ser:
         line =ser.readline()
         print(line)
+def configuration(port):
+    with serial.Serial(port, 115200, timeout=3) as ser:
 
 if  __name__ == '__main__':
-    portname =
+    if platform == 'win32':
+        dataPorts = ['COM6', 'COM9']
+        configPorts = ['COM7', 'COM6']
+    elif platform == 'linux':
+        dataPorts = ['/usb/..', '/usb/..']
+        configPorts = ['/usb/..', '/usb/..']
+
