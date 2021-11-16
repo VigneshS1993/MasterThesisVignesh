0ede534451e0ab98d25939ba73b6227a7f935adc
Vignesh_S <snesh93@gmail.com>
2021-11-16 15:44:32 +0100

    Initial files and sub files

--- parallelism.py
+++ parallelism.py
@@ -2,3 +2,13 @@
 
 import threading
 import multiprocessing
+import serial
+
+
+def portCommunication(portName):
+    with serial.Serial(portName, 921600, timeout=3) as ser:
+        line =ser.readline()
+        print(line)
+
+if  __name__ == '__main__':
+    portname =
