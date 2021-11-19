842c4670daeadc8c0dee18fc430df887ecc8961d
Vignesh_S <snesh93@gmail.com>
2021-11-19 14:53:40 +0100

    Small working of the parallelism using multiprocessing- two ports

--- parallelism.py
+++ parallelism.py
@@ -6,13 +6,21 @@ from serial import Serial as Se
 from sys import platform
 from serialDataParsing import parser_one_mmw_demo_output_packet
 
+def keyPress(key):
+    if key == keyboard.Key.esc:
+        return True
+    else:
+        return False
 
-def readData(port):
+def readData(port, q):
     with Se(port, baudrate=921600, timeout=3) as ser:
         #ser.open()
         line = ser.readline()
-        #q.put(line)
-        print(f"Data from the port {port} line", line)
+        length = len(line)
+        q.put(line)
+        print(f"Length of the line is {length} and the data from the port {port} line ", line)
+        #result, headerStartIndex, totalPacketNumBytes, numDetObj, numTlv, subFrameNumber, detectedX_array, detectedY_array, detectedZ_array, detectedV_array, detectedRange_array, detectedAzimuth_array, detectedElevAngle_array, detectedSNR_array, detectedNoise_array = parser_one_mmw_demo_output_packet(line, len(line))
+        #print("The number of objects detected in this port are", numDetObj)
         ser.close()
 #def configuration(port):
 #    with serial.Serial(port, 115200, timeout=3) as ser:
@@ -25,20 +33,22 @@ if  __name__ == '__main__':
         dataPorts = ['/usb/..', '/usb/..']
         configPorts = ['/usb/..', '/usb/..']
 
-    q = mp.Queue()
-    p1 = mp.Process(target=readData, args=('COM9',))
-    p2 = mp.Process(target=readData, args=('COM6',))
-    p1.start()
-    p2.start()
+    try:
+        while True:
+            q1 = mp.Queue()
+            q2 = mp.Queue()
+            p1 = mp.Process(target=readData, args=('COM9',q1))
+            p2 = mp.Process(target=readData, args=('COM6',q2))
+            p1.start()
+            p2.start()
+            p1.join()
+            p2.join()
+            serialData = []
+            ## Not able to acheive the q.empty logic for getting all the values from the queue
+            serialData.append(q1.get())
+            serialData.append(q2.get())
+            print("The appended serial data is ", serialData)
 
-    p1.join()
-    p2.join()
-
-    #serialData = []
-
-    """while not q.empty() == False:
-        data = q.get()
-        serialData.append(data)
-
-    print(serialData)"""
+    except KeyboardInterrupt:
+        exit()
 
