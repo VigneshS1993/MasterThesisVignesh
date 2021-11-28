8074da41c54b96c173e289d092e9eddc760d40a2
Vignesh_S <snesh93@gmail.com>
2021-11-28 11:11:08 +0100

    updated the rawdata synthesis

--- rawDataSynthesis.py
+++ rawDataSynthesis.py
@@ -1,2 +1,72 @@
 """ This program will help us to decode the raw data received from the serial ports (radars) """
+import serial
+import time
+import numpy as np
 
+def parseConfigFile(configFileName):
+    configParameters = {}
+    config = [line.rstrip('\n') for line in open(configFileName)]
+    for i in config:
+
+        splitWords = i.split(" ")
+        # Split lines
+
+        #Hard code number of antennas, change if other configuration is used
+        numRxAnt = 4
+        numTxAnt = 3
+
+        # Get the information about the profile configuration
+        if "profileCfg" in splitWords[0]:
+            startFreq = int(float(splitWords[2]))
+            idleTime = int(splitWords[3])
+            rampEndTime = float(splitWords[3])
+            freqSlopeConst = float(splitWords[8])
+            numAdcSamples = int(splitWords[10])
+            numAdcSamplesRoundTo2 = 1
+
+            while numAdcSamples > numAdcSamplesRoundTo2:
+                numAdcSamplesRoundTo2 = numAdcSamplesRoundTo2 * 2
+
+            digOutSampleRate = int(splitWords[11])
+
+        # Get the information about the frame configuration
+        elif "frameCfg" in splitWords[0]:
+            chirpStartIdx = int(splitWords[1])
+            chirpEndIdx = int(splitWords[2])
+            numLoops = int(splitWords[3])
+            numFrames = int(splitWords[4])
+            framePeriodicity = int(splitWords[5])
+
+    # Combine the read data to obtain the configuration parameters
+    numChirpsPerFrame = (chirpEndIdx - chirpStartIdx + 1) * numLoops
+    configParameters["numDopplerBins"] = numChirpsPerFrame / numTxAnt
+    configParameters["numRangeBins"] = numAdcSamplesRoundTo2
+    configParameters["rangeResolutionMeters"] = (3e8 * digOutSampleRate * 1e3)
+    configParameters["rangeIdxToMeters"] = (3e8 * digOutSampleRate * 1e3) / (2 * freqSlopeConst * 1e12 * configParameters["numRangeBins"])
+    configParameters["dopplerResolutionMps"] = 3e8 / (2 * startFreq * 1e9) * (idleTime + rampEndTime) * 1e-6 * configParameters["numDopplerBins"] * numTxAnt)
+    configParameters["maxRange"] = (300 * 0.9 * digOutSampleRate) / (2 * freqSlopeConst * 1e3)
+    configParameters["maxVelocity"] = 3e8 / (4 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * numTxAnt)
+
+    return configParameters
+
+def readAndParseData(serialData, configParameters):
+    global byteBuffer, byteBufferLength
+
+    #Constants
+
+    OBJ_STRUCT_SIZE_BYTES = 12
+    BYTE_VEC_ACC_MAX_SIZE = 2**15
+    MMWDEMO_UART_MSG_DETECTED_POINTS = 1
+    MMWDEMO_UART_MSG_RANGE_PROFILE = 2
+    maxBufferSize = 2**15
+    magicWord = [2, 1, 4, 3, 6, 5, 8, 7]
+
+    #Initialize variables
+
+    magicOK = 0 # Checks if magic number has been read
+    dataOK = 0 # checks if the data has been read correctly
+    frameNumber = 0
+    detObj = {}
+
+    byteVec = np.frombuffer(serialData, dtype = 'uint8')
+    byteCount = len(byteVec)
